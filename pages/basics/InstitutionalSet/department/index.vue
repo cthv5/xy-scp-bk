79dea1f40fd7a4f36f31ebfdcba46df6293014b6
sam <sam@thinkingsam.cn>
2019-05-23 17:22:30 +0800

    text clrf

--- pages/basics/InstitutionalSet/department/index.vue
+++ pages/basics/InstitutionalSet/department/index.vue
@@ -8,7 +8,7 @@ export default {
   components: {
     basicPanel
   },
-  data () {   
+  data() {
     return {
       basicPanel: {
         rowKey: 'deptCode',
@@ -16,58 +16,106 @@ export default {
           type: 'search',
           title: '查询',
           formItem: [
-            {lbl: '部门名称', model: 'deptName'},
-            {lbl: '部门代码', model: 'deptCode'}          
+            { lbl: '部门名称', model: 'deptName' },
+            { lbl: '部门代码', model: 'deptCode' }
           ]
-        }, 
-        rowSelection: {type: 'radio'},
+        },
+        rowSelection: { type: 'radio' },
         buttonGroup: [
-          {type: 'add', icon: 'plus', text: '增加'}, 
-          {type: 'edit', icon: 'edit', text: '编辑'}, 
-          {type: 'del', icon: 'delete', text: '删除', popconfirm: {title: '确认删除？'}}, 
-          {type: 'reload', icon: 'reload', text: '刷新'}
+          { type: 'add', icon: 'plus', text: '增加' },
+          { type: 'edit', icon: 'edit', text: '编辑' },
+          {
+            type: 'del',
+            icon: 'delete',
+            text: '删除',
+            popconfirm: { title: '确认删除？' }
+          },
+          { type: 'reload', icon: 'reload', text: '刷新' }
         ],
         columns: [
-          {title: '部门名称', dataIndex: 'deptName', key: 'deptName'}, 
-          {title: '部门代码', dataIndex: 'deptCode', key: 'deptCode', width: 150}, 
-          {title: '负责人', dataIndex: 'deptManager', key: 'deptManager', width: 150}, 
-          {title: '统计部门', key: 'deptTjname', dataIndex: 'deptTjname', width: 150}, 
-          {title: '销售统计部门', key: 'deptSale', dataIndex: 'deptSale', width: 200}, 
-          {title: '状态', dataIndex: 'deptState', key: 'deptState', width: 80}, 
-          {title: '备注', dataIndex: 'deptRemark', key: 'deptRemark', width: 350}
+          { title: '部门名称', dataIndex: 'deptName', key: 'deptName' },
+          {
+            title: '部门代码',
+            dataIndex: 'deptCode',
+            key: 'deptCode',
+            width: 150
+          },
+          {
+            title: '负责人',
+            dataIndex: 'deptManager',
+            key: 'deptManager',
+            width: 150
+          },
+          {
+            title: '统计部门',
+            key: 'deptTjname',
+            dataIndex: 'deptTjname',
+            width: 150
+          },
+          {
+            title: '销售统计部门',
+            key: 'deptSale',
+            dataIndex: 'deptSale',
+            width: 200
+          },
+          {
+            title: '状态',
+            dataIndex: 'deptState',
+            key: 'deptState',
+            width: 80
+          },
+          {
+            title: '备注',
+            dataIndex: 'deptRemark',
+            key: 'deptRemark',
+            width: 350
+          }
         ],
-        data: [{
-          deptName: '测试部门1',
-          deptCode: '000053',
-          deptManager: '测试部门1',
-          deptTjname: '测试部门',
-          deptSale: '开发一部',
-          deptState: '启用',
-          deptRemark: '-----------1'   
-        }, {
-          deptName: '测试部门1',
-          deptCode: '000054',
-          deptManager: '测试部门1',
-          deptTjname: '测试部门',
-          deptSale: '开发一部',
-          deptState: '启用',
-          deptRemark: '-----------1'    
-        }],
+        data: [
+          {
+            deptName: '测试部门1',
+            deptCode: '000053',
+            deptManager: '测试部门1',
+            deptTjname: '测试部门',
+            deptSale: '开发一部',
+            deptState: '启用',
+            deptRemark: '-----------1'
+          },
+          {
+            deptName: '测试部门1',
+            deptCode: '000054',
+            deptManager: '测试部门1',
+            deptTjname: '测试部门',
+            deptSale: '开发一部',
+            deptState: '启用',
+            deptRemark: '-----------1'
+          }
+        ],
         formItem: [
-          {lbl: '部门名称', decorator: ['deptName', {rules: [{ required: true, message: '请输入部门名称' }]}]}, 
-          {lbl: '负责人', decorator: ['deptManager']}, 
-          {lbl: '统计部门', decorator: ['deptTjname']}, 
-          {lbl: '销售统计部门', decorator: ['deptSale']}, 
-          {lbl: '状态', decorator: ['deptState']}, 
-          {lbl: '备注', decorator: ['deptRemark'], type: 'textarea', col: 24, labelCol: 2}
+          {
+            lbl: '部门名称',
+            decorator: [
+              'deptName',
+              { rules: [{ required: true, message: '请输入部门名称' }] }
+            ]
+          },
+          { lbl: '负责人', decorator: ['deptManager'] },
+          { lbl: '统计部门', decorator: ['deptTjname'] },
+          { lbl: '销售统计部门', decorator: ['deptSale'] },
+          { lbl: '状态', decorator: ['deptState'] },
+          {
+            lbl: '备注',
+            decorator: ['deptRemark'],
+            type: 'textarea',
+            col: 24,
+            labelCol: 2
+          }
         ]
-      }      
+      }
     }
-  },  
+  },
   methods: {
     tableCb(type) {
-      console.log('---------1')
-      console.log(type)
       this.$message.success('删除成功')
     }
   }
